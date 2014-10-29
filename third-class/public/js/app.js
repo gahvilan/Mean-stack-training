4d73cd9f24f20c9ee75a7bc351dd26f3bc686de2
Pablo Saavedra <psaavedra@devspark.com>
2014-10-29 16:49:49 -0300

    Draft of the angularjs class

---
+++ third-class/public/js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('sampleApp',[]);
+
+app.controller('postsController', function($scope) {
+    $scope.title = 'Devspark Test App';
+    $scope.data = [];
+
+    $scope.addText = function() {
+        $scope.data.push({
+            text : $scope.inputText,
+            score:0
+        });
+        $scope.inputText = '';
+    };
+
+    $scope.removeText = function(dataInput) {
+        $scope.data.splice($scope.data.indexOf(dataInput),1);
+    };
+
+    $scope.increaseScore = function(dataInput) {
+        dataInput.score++;
+    };
+
+    $scope.decreaseScore = function(dataInput) {
+        if (dataInput.score > 0) {
+            dataInput.score--;
+        }
+    };
+});
